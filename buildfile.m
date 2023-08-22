54d12a1039429856eb6d84c54653c5cfb12eea3f
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-22 13:07:49 +0200

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -4,13 +4,14 @@ plan.DefaultTasks = "archive";
 plan("archive").Dependencies = ["check" "test"];
 end
 
+%{
 function checkTask(~)
 % Identify code issues
 issues = codeIssues;
 assert(isempty(issues.Issues),formattedDisplayText( ...
     issues.Issues(:,["Location" "Severity" "Description"])))
 end
-
+%}
 function testTask(~)
 % Run unit tests
 results = runtests(IncludeSubfolders=true,OutputDetail="terse");

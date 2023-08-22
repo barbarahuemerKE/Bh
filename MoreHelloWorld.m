bbf641a36dd03c92a459f888ce950cab6a1995cc
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-22 13:53:49 +0200

    Update MoreHelloWorld.m

--- MoreHelloWorld.m
+++ MoreHelloWorld.m
@@ -1,10 +1,8 @@
-function helloworlds()
-  fprintf('\nHello main\n');
-end
-
-
 function main 
   fprintf('\nHello funktion\n');
   helloworlds();
 end
 
+function helloworlds()
+  fprintf('\nHello main\n');
+end

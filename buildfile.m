5cb0dc973f1733dbba07d391acb4ceaa507113f8
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-22 13:07:03 +0200

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -17,9 +17,11 @@ results = runtests(IncludeSubfolders=true,OutputDetail="terse");
 assertSuccess(results);
 end
 
+%{
 function archiveTask(~)
 % Create ZIP file
 zipFileName = "source_" + ...
     string(datetime("now",Format="yyyyMMdd'T'HHmmss"));
 zip(zipFileName,"*")
 end
+%}

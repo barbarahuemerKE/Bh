699abeffe5850f0a815aaccf93ba6c6695cea4cb
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-25 11:37:49 +0200

    Update build_lib.m

--- build_lib.m
+++ build_lib.m
@@ -1,4 +1,10 @@
-% Load the test data
 %load simulink.mat
-% Generate code for euclidean.m with codegen. Use the test data as example input.
+oldpath = path; %retrieve matlab searchpath
+path(oldpath,'C:\ProgramData\Jenkins\.jenkins\workspace') %copys jenkins workspace in the Mathlab searchpath.
+
+cfg = Simulink.fileGenControl('getConfig');
+cfg.CacheFolder = fullfile(eval(['O:\github\generatedCache']),'generatedCache');
+cfg.CodeGenFolder = fullfile(eval(['O:\github\generatedCode']),'generatedCode');
+Simulink.fileGenControl('setConfig', 'config', cfg,'createDir',true);
 codegen -report -config:lib simulink.m %-args {x, cb}
+path(oldpath) %restores initial path

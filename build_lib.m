d8590d5d1406ac35f98885594944a77361771925
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-28 11:30:42 +0200

    Update build_lib.m

--- build_lib.m
+++ build_lib.m
@@ -1,12 +1,14 @@
-fprintf('HelloWorld1');
-cfg = Simulink.fileGenControl('getConfig');
-cfg.CacheFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink','generatedCache');
-cfg.CodeGenFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink','generatedCode');
-Simulink.fileGenControl('setConfig', 'config', cfg,'createDir',true);
-
-
-model= 'simulink';
-open_system(model)
-cs = getActiveConfigSet(model);
-%openDialog(cs);
-slbuild(model); %,'ModelReferenceCoderTarget'
+function codegeneration
+    fprintf('HelloWorld1');
+    cfg = Simulink.fileGenControl('getConfig');
+    cfg.CacheFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink','generatedCache');
+    cfg.CodeGenFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink','generatedCode');
+    Simulink.fileGenControl('setConfig', 'config', cfg,'createDir',true);
+    
+    
+    model= 'simulink';
+    open_system(model)
+    cs = getActiveConfigSet(model);
+    %openDialog(cs);
+    slbuild(model); %,'ModelReferenceCoderTarget'
+end

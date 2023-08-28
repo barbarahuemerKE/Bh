3b102a275ccaf8408a088e6a64368e58cfda917a
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-28 08:48:19 +0200

    Update build_lib.m

--- build_lib.m
+++ build_lib.m
@@ -1,10 +1,5 @@
-%load simulink.mat
-oldpath = path; %retrieve matlab searchpath
-path(oldpath,'C:\ProgramData\Jenkins\.jenkins\workspace') %copys jenkins workspace in the Mathlab searchpath.
-
-cfg = Simulink.fileGenControl('getConfig');
-cfg.CacheFolder = fullfile(eval(['C:\ProgramData\Jenkins\.jenkins\workspace\simulink\codegen\lib\simulink\html']),'generatedCache');
-cfg.CodeGenFolder = fullfile(eval(['C:\ProgramData\Jenkins\.jenkins\workspace\simulink\codegen\lib\simulink\examples']),'generatedCode');
-Simulink.fileGenControl('setConfig', 'config', cfg,'createDir',true);
-codegen -report -config:lib simulink.m %-args {x, cb}
-path(oldpath) %restores initial path
+model= 'simulink';
+%open_system(model)
+cs = getActiveConfigSet(model);
+%openDialog(cs);
+slbuild(model); %,'ModelReferenceCoderTarget'

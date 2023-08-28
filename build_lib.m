c4e573bcd036be66adb37a31b63f9b0f88a9f92c
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-28 11:22:13 +0200

    Update build_lib.m

--- build_lib.m
+++ build_lib.m
@@ -1,6 +1,6 @@
 cfg = Simulink.fileGenControl('getConfig');
-cfg.CacheFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink\generatedCache','generatedCache');
-cfg.CodeGenFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink\generatedCode','generatedCode');
+cfg.CacheFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink','generatedCache');
+cfg.CodeGenFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink','generatedCode');
 Simulink.fileGenControl('setConfig', 'config', cfg,'createDir',true);
 
 

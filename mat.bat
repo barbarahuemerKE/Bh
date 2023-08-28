6dbcf413bb2a891b7ece005beb397342ade22d7d
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-28 14:11:53 +0200

    Update mat.bat

--- mat.bat
+++ mat.bat
@@ -1,4 +1,4 @@
 SET MATLABROOT="C:\Program Files\MATLAB\R2023a"
 PATH=%MATLABROOT%;%PATH%
-START matlab.exe -batch build_lib %1 -logfile c:\temp\logfile
+START call matlab.exe -batch build_lib %1 -logfile c:\temp\logfile
 PAUSE

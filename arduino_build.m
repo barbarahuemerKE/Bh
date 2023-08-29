8c7c11b3b56c08c905f05eddff050f921f6b1ae1
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-29 09:53:49 +0200

    Update arduino_build.m

--- arduino_build.m
+++ arduino_build.m
@@ -1,18 +1,1 @@
-%% Build Process
-bdclose('all');
-model = 'simulink';
-% Open the system
-open_system(model);
-% 
-cs = getActiveConfigSet(model);
-cs.set_param('GenCodeOnly', getenv('GEN_CODE_ONLY')); 
-% Build the model
-slbuild(model);
 
-% Save the system and close it.
-% Saves are not pushed back to the version control but it eliminates a
-% dialog prompt that pauses the build process.
-save_system(model);
-close_system(model, 1);
-% Close out Models.
-bdclose('all');

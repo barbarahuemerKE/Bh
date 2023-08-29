187017067a438c18dc76e41654c19a7a6d824eea
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-29 09:00:48 +0200

    Update build_lib.m

--- build_lib.m
+++ build_lib.m
@@ -1,15 +1,12 @@
 function codegeneration
-    fprintf('HelloWorld1');
-    cfg = Simulink.fileGenControl('getConfig');
+    cfg = Simulink.fileGenControl('getConfig'); %specify the current working directory, create two folders 'generatedCache' and 'generatedCode' in the current directory, and set these as the target folders for all code generation.
     cfg.CacheFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink','generatedCache');
     cfg.CodeGenFolder = fullfile('C:\ProgramData\Jenkins\.jenkins\workspace\simulink','generatedCode');
     Simulink.fileGenControl('setConfig', 'config', cfg,'createDir',true);
     
-    fprintf('HelloWorld2');
     model= 'simulink';
     open_system(model)
-    fprintf('HelloWorld3');
     cs = getActiveConfigSet(model);
     %openDialog(cs);
-    slbuild(model); %,'ModelReferenceCoderTarget'
+    slbuild(model); 
 end

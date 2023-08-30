1fa01a06ea4d40ca976554dc0848fbcead65788d
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-30 15:35:27 +0200

    Update mat.bat

--- mat.bat
+++ mat.bat
@@ -1,5 +1,5 @@
 setlocal enabledelayedexpansion
 SET MATLABROOT="C:\Program Files\MATLAB\R2023a"
 PATH=%MATLABROOT%;%PATH%
-START call matlab.exe -batch "build_lib" %1 -logfile c:\temp\logfile
+START call C:\Program Files\MATLAB\R2023a\bin\matlab.exe -batch "build_lib" %1 -logfile c:\temp\logfile
 PAUSE

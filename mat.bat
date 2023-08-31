a35a8d45b5b56438429b9039cdf072f616045f20
barbarahuemerKE <142782613+barbarahuemerKE@users.noreply.github.com>
2023-08-31 14:17:14 +0200

    Update mat.bat

--- mat.bat
+++ mat.bat
@@ -1,4 +1,3 @@
-setlocal enabledelayedexpansion
 SET MATLABROOT="C:/Program Files/MATLAB/R2023a"
 PATH=%MATLABROOT%;%PATH%
 matlab -nosplash -noFigureWindows -wait -r "try; run('C:/ProgramData/Jenkins/.jenkins/workspace/simulink/build_lib.m'); catch; end; quit"
